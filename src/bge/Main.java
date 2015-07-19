2354da92f884fe38f14ea6c53892a5a820a38156
y7haar <y7haar@gmail.com>
2015-07-19 13:50:04 +0200

    Added Color class

--- src/bge/Main.java
+++ src/bge/Main.java
@@ -1,12 +1,15 @@
 package bge;
 
 import bge.core.Engine;
-import bge.core.Input;
+import bge.core.Color;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        Color c = Color.CLEAR;
+
+        System.out.println(c);
 
         Engine engine = Engine.getInstance();
         engine.run();
